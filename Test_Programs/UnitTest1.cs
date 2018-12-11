6c383cac67fe2cb680dbff2fb31c335adebda897
kousokujin <kousokujin@outlook.jp>
2018-12-11 13:13:33 +0900

    テストプロジェクト作成

---
+++ Test_Programs/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Karnaugh_Logic;
+
+namespace Test_Programs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
